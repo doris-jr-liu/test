7c2c1e47ef8aac115340948a458168f19172e241
Junru Liu <jl5218@columbia.edu>
2020-07-02 10:19:15 +0800

    'initialization'

---
+++ code_file_1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  1 16:33:53 2020
+
+@author: doris
+"""
+
+print('file 2')
---
+++ code_file_2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  1 16:30:10 2020
+
+@author: doris
+"""
+
+print('file 2')
